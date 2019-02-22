1e49d4421fc857348e7f7d099190239772a700fc
muemmel <tetje@yandex.ru>
2019-02-23 00:30:32 +0100

    Updated and bugs removed

--- x_to_opus_org.py
+++ x_to_opus_org.py
@@ -1,10 +1,11 @@
-# import multiprocessing as mp
-import getpass
-import multiprocessing.dummy as mp_thread
+import concurrent.futures as cf
 import os
+import os.path as p
 import subprocess
-import typing as tp
 import ffmpy
+import argparse
+import typing as tp
+
 """
 convertes all audio, movie files
 of a given root directory into .opus
@@ -12,91 +13,84 @@ of a given root directory into .opus
 requires: ffmpeg incl. libopus
 """
 
-def isvideo(file: str)->bool:
-    """
-    test whether param file is has a known ending for video formats
-    :param file: str
-    :return: bool
-    """
-    video_files = ["avi", "mp4", "webm"]
-    end = file.rsplit(".", 1)[-1]
-    return True if end in video_files else False
 
-
-def isaudio(file: str)->bool:
-    """
-    test whether param file is has a known ending for audio formats
-    :param file: str
-    :return: bool
-    """
-    audio_files = ["mp3", "aac", "ogg", "m4a"]
-    end = file.rsplit(".", 1)[-1]
-    return True if end in audio_files else False
-
-
-def create_io_struct_list(in_files: list, start_name_fn: tp.Callable[[str, tp.Optional[str]], str],
-                          end_name_fn: tp.Callable[[str, str, str], str], out_path_dir: tp.Optional[str])->list:
-    """
-    create a list containing a list of in and out file names
-    :type in_files: list
-    :type start_name_fn: function
-    :type end_name_fn: function
-    :type out_path_dir: str
-    :return: list
-    """
-    media_files = list(filter(lambda x: (isaudio(x) or isvideo(x)) and os.path.isfile(x), in_files))
-    return [[x, end_name_fn(start_name_fn(x, out_path_dir), ".ogg", ".opus"), False] for x in media_files]
-
-
-def convert(pool_map_tuple: tuple)->None:
+def convert(file: str)->bool:
     """
     Does the actual conversion
-    :param pool_map_tuple: tuple of list(input file name, output filename, conversion flag),
-     function for codex, function for bit rate
-    :return: None
+    :param file: str of the filename
+    :return: bool: is converted?
     """
-    # unpack
-    file, codec_fn, bit_rate_fn = pool_map_tuple
+
     # calculate the bit_rate_fn bit rate for the new file
-    out_bit_rate: int = 0
-    in_bit_rate = bit_rate_fn(file[0])
-    in_codec = codec_fn(file[0])
-    if codec_fn(file[0]) == "vorbis" or "aac":
-        out_bit_rate = (lambda x: 54000 if x <= 64001 else 64000)(in_bit_rate)
+    in_bit_rate = get_in_bit_rate(file)
+
+    # for codex with higher performances, do not reduce bitrate so strongly
+    if any([x in get_audio_codec(file) for x in (b"vorbis", b"aac", b"opus")]):
+        if in_bit_rate < 54000:
+            out_bit_rate = in_bit_rate
+        elif in_bit_rate <= 64000:
+            out_bit_rate = 54000
+        else:
+            out_bit_rate = 64000
     else:
-        out_bit_rate = (lambda x: 54000 if x <= 100001 else 64000)(in_bit_rate)
-
-    if (in_bit_rate < out_bit_rate) or in_codec == "opus":
-        return
-
-    # set conversion flag
-    file[2] = True
+        if in_bit_rate < 16000:
+            out_bit_rate = in_bit_rate
+        elif in_bit_rate <= 32000:
+            out_bit_rate = 16000
+        elif in_bit_rate <= 48000:
+            out_bit_rate = 32000
+        elif in_bit_rate <= 10000:
+            out_bit_rate = 54000
+        else:
+            out_bit_rate = 64000
 
     # parse param for conversion
     o_command: str = '-v error -vn -vbr constrained -b:a ' + str(out_bit_rate) + \
                      ' -compression_level 10 -acodec libopus'
+
     ff = ffmpy.FFmpeg(
-        inputs={file[0]: None},
-        outputs={file[1]: o_command}
+        inputs={file: None},
+        outputs={rename(file, out_dir): o_command}
     )
 
     # convert
-    ff.run()
+    try:
+        ff.run()
+        return True
+    except ffmpy.FFRuntimeError:
+        return False
 
 
-def get_in_bit_rate(file: str)-> int:
+def _get_ffprobe(file: str, command: str)->tp.Any:
+    """
+    Returns the bitrate of file with help of ffprobe
+    :param file: str, filename
+    :param command: str, command for probing
+    :return: retrieved value from probe
+    """
+    probe = ffmpy.FFprobe(inputs={file: command})
+    try:
+        query = probe.run(stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    except ffmpy.FFRuntimeError:
+        return None
+    except ffmpy.FFExecutableNotFoundError:
+        return None
+    else:
+        return query[0]
+
+
+def get_in_bit_rate(file: str)-> tp.Optional[int]:
     """
     Returns the bitrate of file with help of ffprobe
     :param file: str filename
     :return: int bit rate
     """
     p_command: str = "-v error -select_streams a:0 -show_entries stream=bit_rate -of default=noprint_wrappers=1:nokey=1"
-    probe = ffmpy.FFprobe(inputs={file: p_command})
+    ret_val = _get_ffprobe(file, p_command)
     try:
-        bit_rate = int((probe.run(stdout=subprocess.PIPE))[0])
+        return int(ret_val) if ret_val else None
     except ValueError:
-        bit_rate = -1
-    return bit_rate
+        return None
 
 
 def get_file_names(path: str)->list:
@@ -105,14 +99,31 @@ def get_file_names(path: str)->list:
     :param path: str path to selected root folder
     :return: list
     """
-    file_list: list = []
-    for rootdir, subdirs, files in os.walk(path):
+    files_: list = []
+    for root_dir, _, files in os.walk(path):
         for name in files:
-            file_list.append(rootdir + r"/" + name)
-    return file_list
+            files_.append(root_dir + r"/" + name)
+    return files_
 
 
-def get_in_codec(file: str)-> str:
+class Memorize:
+    """
+    Memorization
+    :param f: function name
+    :return: Any (Anything)
+    """
+    def __init__(self, f):
+        self.f = f
+        self.memo = {}
+
+    def __call__(self, *args):
+        if args not in self.memo:
+            self.memo[args] = self.f(*args)
+        return self.memo[args]
+
+
+@Memorize
+def get_audio_codec(file: str)-> tp.Optional[str]:
     """
     return the audio codec name of file
     :param file: str file name
@@ -120,30 +131,56 @@ def get_in_codec(file: str)-> str:
     """
     # https://github.com/Ch00k/ffmpy/blob/master/docs/examples.rst
     p_command = "-v error -select_streams a:0 -show_entries stream=codec_name -of default=noprint_wrappers=1:nokey=1"
-    probe = ffmpy.FFprobe(inputs={file: p_command})
-    return (probe.run(stdout=subprocess.PIPE))[0].strip()
+    ret_val = _get_ffprobe(file, p_command)
+    return ret_val.strip() if ret_val else None
 
 
-def rename_ending(file: str, condition: str, ending: str)-> str:
+def get_video_codec(file: str)-> tp.Optional[str]:
+    """
+    return the audio codec name of file
+    :param file: str file name
+    :return: str codex name
+    """
+    # https://github.com/Ch00k/ffmpy/blob/master/docs/examples.rst
+    p_command = "-v error -select_streams v:0 -show_entries stream=codec_name -of default=noprint_wrappers=1:nokey=1"
+    ret_val = _get_ffprobe(file, p_command)
+    return ret_val.strip() if ret_val else None
+
+
+@Memorize
+def get_duration(file: str)->tp.Optional[float]:
+    """
+    :return the length of media file in seconds
+    :param file: str Name of the file
+    :return: int duration in seconds
+    """
+    p_command: str = "-v error -show_entries format=duration -of default=noprint_wrappers=1:nokey=1"
+    ret_val = _get_ffprobe(file, p_command)
+
+    return float(ret_val) if ret_val else None
+
+
+def rename(file: str, new_path: str)-> str:
     """
     conditional renaming of file name ending
     :param file: str file name
-    :param condition: str file name ending to be compared with
-    :param ending: str new ending
+    :param new_path: new name of the path
     :return: str
     """
-    file_name = file.rsplit(".", 1)[0]
-    return file_name + ending if file.endswith(condition) else file_name + condition
+
+    def fsplit(x: str)->str:
+        return x.rsplit(".", 1)[0]
+
+    file_name = fsplit(p.basename(file))
+    return p.join(new_path, file_name, ".opus") if new_path else p.join(fsplit(file) + ".opus")
 
 
-def rename_start(file: str, output_dir: tp.Optional[str])-> str:
-    """
-    rename of the root folder for output if that one has been specified
-    :param file: str file name
-    :param output_dir: str new root folder name, empty if nothing shall be done
-    :return: str new name
-    """
-    return output_dir + file.rsplit("/", 1)[-1] if output_dir else file
+def parse_arguments():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('path', type=str, help='Path to the main folder of files.')
+    parser.add_argument('-o', '--output', type=str,
+                        help='Path to the main output folder. Default same as input with override', default=None)
+    return parser.parse_args()
 
 
 if __name__ == '__main__':
@@ -151,35 +188,43 @@ if __name__ == '__main__':
     changes audio and video files to .opus files only
     in specified directory and all it's subdirectories
     """
-    p_dir = "/home/" + getpass.getuser() + "/Downloads/Hörbücher"
-    out_dir: str = None
+    arguments = parse_arguments()
+    p_dir = arguments.path
+    out_dir = arguments.output
 
     # files will contain all files to be changed
     # structure list of (in_file_name, out_filename, conversion_flag)
-    all_files = get_file_names(p_dir)
-    in_out_files: list = create_io_struct_list(all_files, rename_start, rename_ending, out_dir)
-
-    # sorting files?
+    in_files = get_file_names(p_dir)
 
     # doing the slow stuff in parallel
-
     # http.//chriskiehl.com/article-parallelism-in-one-line/
     # open Pool with mp_thread.cpu_count() cores
-    pool = mp_thread.Pool()
 
-    # pool.map can only work on one iterable
-    # create on iterable
-    pool_iterable = [(x, get_in_codec, get_in_bit_rate) for x in in_out_files]
-    pool.map(convert, pool_iterable)
-    pool.close()
-    pool.join()
+    with cf.ThreadPoolExecutor() as executor:
+        file_dict = dict(
+            zip(in_files, list(executor.map(get_audio_codec, in_files))))
 
-    # return to serial processing
+    # deselect all cases that, do are not needed to be converted
+    # has no audio, or audio is already opus(that has no video)
+    file_set = {k for (k, v) in file_dict.items() if v}
+    opus_set = {k for (k, v) in file_dict.items() if v == b"opus" and not get_video_codec(k)}
+    file_list = list(file_set - opus_set)
+
+    # convert
+    # test whether all important facts are known?
+    with cf.ThreadPoolExecutor() as executor:
+        length_dict = dict(zip(file_list, list(executor.map(get_duration, file_list))))
+        length_list = [k for (k, v) in length_dict.items() if v]
+
+        rate_dict = dict(zip(file_list, list(executor.map(get_in_bit_rate, file_list))))
+        rate_list = [k for (k, v) in rate_dict.items() if v]
+
+        is_converted_dict = dict(zip(length_list, list(executor.map(convert, rate_list))))
+
     # delete all files that have been converted
-    [os.remove(x[0]) for x in list(filter(lambda x: x[2], in_out_files))]
-    # prepare renaming
-    left_list = list(filter(lambda x: (isvideo(x) or isaudio(x)) and os.path.isfile(x), get_file_names(p_dir)))
-    name_list = map(lambda x: rename_ending(x, ".opus", ".ogg"), left_list)
-
-    # rename
-    map(lambda x, y: os.rename(x, y), left_list, name_list)
+    # for all converted files test whether converted successfully (files lengths old vs new almost equal?)
+    # yes: delete old, no: delete new
+    [os.remove(k)
+     if abs(get_duration(k) - get_duration(rename(k, out_dir))) - 0.1 < 0.0
+     else os.remove(rename(k, out_dir))
+     for (k, v) in is_converted_dict.items() if v]
